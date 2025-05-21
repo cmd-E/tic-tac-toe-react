45030534472984e484c07527b869d91aa318b48b
cmd_e <cmd_e@example.com>
2025-05-21 16:03:36 +0500

    Implement moves sorting

--- src/Game.jsx
+++ src/Game.jsx
@@ -4,11 +4,16 @@ import Board from "./Board";
 export default function Game() {
 	const [history, setHistory] = useState([Array(9).fill(null)])
 	const [currentMove, setCurrentMove] = useState(0)
+	const [isAscSorting, setIsAscSorting] = useState(true)
 	const xIsNext = currentMove % 2 === 0
 	const currentSquares = history[currentMove]
 
 	function jumpTo(nextMove) {
 		setCurrentMove(nextMove)
+	}
+
+	function sortMoves() {
+		setIsAscSorting(!isAscSorting)
 	}
 
 	const moves = history.slice(0, currentMove + 1).map((squares, move) => {
@@ -19,6 +24,10 @@ export default function Game() {
 			</li>
 		);
 	})
+
+	const displayedMoves = isAscSorting ? moves : moves
+		.slice()
+		.reverse()
 
 
 	function handlePlay(nextSquares) {
@@ -33,8 +42,9 @@ export default function Game() {
 				<Board currentSquares={currentSquares} onPlay={handlePlay} xIsNext={xIsNext} />
 			</div>
 			<div className="game-info">
-				<p>Game info</p>
-				<ol>{moves}</ol>
+				<p>Game info:</p>
+				<ol>{displayedMoves}</ol>
+				<p>Sorting: <button onClick={sortMoves}>{isAscSorting ? 'DESC' : 'ASC'}</button></p>
 			</div>
 		</div>
 	)
