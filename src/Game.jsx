52800307f16b0e91360a9ae25dd152f9d090f7ab
cmd_e <cmd_e@example.com>
2025-05-21 16:08:19 +0500

    Refactor code, add disabled on current move

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,40 +1,41 @@
-import { useState } from 'react'
-import Board from "./Board";
+import { useState, useMemo } from 'react';
+import Board from './Board';
 
 export default function Game() {
-	const [history, setHistory] = useState([Array(9).fill(null)])
-	const [currentMove, setCurrentMove] = useState(0)
-	const [isAscSorting, setIsAscSorting] = useState(true)
-	const xIsNext = currentMove % 2 === 0
-	const currentSquares = history[currentMove]
+	const [history, setHistory] = useState([Array(9).fill(null)]);
+	const [currentMove, setCurrentMove] = useState(0);
+	const [isAscSorting, setIsAscSorting] = useState(true);
 
-	function jumpTo(nextMove) {
-		setCurrentMove(nextMove)
+	const xIsNext = currentMove % 2 === 0;
+	const currentSquares = history[currentMove];
+
+	function handlePlay(nextSquares) {
+		const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+		setHistory(nextHistory);
+		setCurrentMove(nextHistory.length - 1);
+	}
+
+	function jumpTo(move) {
+		setCurrentMove(move);
 	}
 
 	function sortMoves() {
-		setIsAscSorting(!isAscSorting)
+		setIsAscSorting(value => !value);
 	}
 
-	const moves = history.slice(0, currentMove + 1).map((squares, move) => {
-		let description = move > 0 ? `Go to move # ${move}` : 'Go to game start';
-		return (
-			<li key={move}>
-				<button onClick={() => jumpTo(move)}>{description}</button>
-			</li>
-		);
-	})
-
-	const displayedMoves = isAscSorting ? moves : moves
-		.slice()
-		.reverse()
-
-
-	function handlePlay(nextSquares) {
-		const nextHistory = [...history.slice(0, currentMove + 1), nextSquares]
-		setHistory(nextHistory)
-		setCurrentMove(nextHistory.length - 1)
-	}
+	const displayedMoves = useMemo(() => {
+		const moves = history.slice(0, currentMove + 1).map((_, move) => {
+			const description = move > 0 ? `Go to move #${move}` : 'Go to game start';
+			return (
+				<li key={move}>
+					<button onClick={() => jumpTo(move)} disabled={move === currentMove}>
+						{description}
+					</button>
+				</li>
+			);
+		});
+		return isAscSorting ? moves : moves.slice().reverse();
+	}, [history, currentMove, isAscSorting]);
 
 	return (
 		<div className="game">
@@ -43,9 +44,14 @@ export default function Game() {
 			</div>
 			<div className="game-info">
 				<p>Game info:</p>
+				<p>
+					Sorting:
+					<button onClick={sortMoves}>
+						{isAscSorting ? 'DESC' : 'ASC'}
+					</button>
+				</p>
 				<ol>{displayedMoves}</ol>
-				<p>Sorting: <button onClick={sortMoves}>{isAscSorting ? 'DESC' : 'ASC'}</button></p>
 			</div>
 		</div>
-	)
+	);
 }
